5a6c0da82a29eeec7f00f63f50533765f6f2937c
Ollie Edwards <oliver.s.edwards@gmail.com>
2015-12-01 22:22:22 +0000

    Add dummy specs

---
+++ spec/wordCounterSpec.js
@@ -0,0 +1,14 @@
+describe("wordCounter", () => {
+    "use strict";
+
+    let fixture;
+
+    beforeEach(() => {
+        fixture = {};
+    });
+
+    it("should count words", () => {
+       expect(fixture).not.toBeNull();
+   });
+
+});
