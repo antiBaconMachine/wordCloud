7535ec6b8ec926ad2d080d95b5a7a4408ea9ca11
Ollie Edwards <oliver.s.edwards@gmail.com>
2015-12-05 18:02:29 +0000

    rough layout with some crappy vertical text

--- public/js/client.js
+++ public/js/client.js
@@ -6,7 +6,23 @@ define(['Ractive', 'jquery', 'text!views/domCloud.html', 'json!res/topics.json',
         el: 'content',
         template: domCloudTemplate,
         data: {
-            topics: _.shuffle(proc.weight.bottomHeavy(6, json.topics.sort(proc.sort.hilo)))
+            topics: _.shuffle(proc.weight.bottomHeavy(6, json.topics.sort(proc.sort.hilo))),
+            pad: _.compose(Math.floor, function(x) {
+                return x * 30
+            }, Math.random),
+            verticalAlign: function() {
+                var rnd = Math.floor(Math.random() * 3);
+                if (rnd === 2) {
+                    return "top";
+                } else if (rnd === 1) {
+                    return "middle";
+                } else {
+                    return "bottom";
+                }
+            },
+            vertical: function() {
+                return (Math.floor(Math.random() * 10) === 0) ? 'vertical' : '';
+            }
         }
     });
 
