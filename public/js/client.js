9100619d909f6981e83d3b469521a65d19f2ed56
Ollie Edwards <oliver.s.edwards@gmail.com>
2015-12-07 13:21:44 +0000

    Dummy sidebar chart

--- public/js/client.js
+++ public/js/client.js
@@ -1,6 +1,10 @@
-define(['Ractive', 'jquery', 'text!views/domCloud.html', 'json!res/topics.json', 'topicProcessor', 'lodash', 'config', 'text!views/layout.html', 'text!views/sideBar.html'],
-    function (Ractive, $, tDomCloud, json, proc, _, config, tLayout, tSideBar) {
+define(['Ractive', 'jquery', 'text!views/domCloud.html', 'json!res/topics.json', 'topicProcessor', 'lodash', 'config', 'text!views/layout.html', 'text!views/sideBar.html', 'd3', 'd3pie'],
+    function (Ractive, $, tDomCloud, json, proc, _, config, tLayout, tSideBar, d3, d3pie) {
         "use strict";
+
+        var CONST = Object.freeze({
+            donutId: "donut"
+        });
 
         Ractive.DEBUG = false;
         var ractive = new Ractive({
@@ -43,7 +47,163 @@ define(['Ractive', 'jquery', 'text!views/domCloud.html', 'json!res/topics.json',
         });
 
         ractive.on('selectTopic', function(event) {
-            ractive.set('focussed', $(event.node).data('topic'));
+            var id = $(event.node).data('topic');
+            ractive.set('focussed', id);
+            donut(id);
         });
+
+        var donut = function(id) {
+            $('#' + CONST.donutId).html('');
+            new d3pie("donut", {
+                "header": {
+                    "title": {
+                        "text": "Top 15 Fears",
+                        "fontSize": 34,
+                        "font": "courier"
+                    },
+                    "subtitle": {
+                        "text": "What strikes the most terror in people?",
+                        "color": "#999999",
+                        "fontSize": 10,
+                        "font": "courier"
+                    },
+                    "location": "pie-center",
+                    "titleSubtitlePadding": 10
+                },
+                "footer": {
+                    "text": "* This was curious. We're not sure why over several people regard Winnipeg as a Top 15 Fear.",
+                    "color": "#999999",
+                    "fontSize": 10,
+                    "font": "open sans",
+                    "location": "bottom-left"
+                },
+                "size": {
+                    "canvasHeight": 300,
+                    "canvasWidth": 300,
+                    "pieInnerRadius": "80%",
+                    "pieOuterRadius": "70%"
+                },
+                "data": {
+                    "sortOrder": "label-desc",
+                    "content": [
+                        {
+                            "label": "Spiders",
+                            "value": 2,
+                            "color": "#333333"
+                        },
+                        {
+                            "label": "Mother-in-laws",
+                            "value": 10,
+                            "color": "#444444"
+                        },
+                        {
+                            "label": "Sharks",
+                            "value": 8,
+                            "color": "#555555"
+                        },
+                        {
+                            "label": "Alien invasion",
+                            "value": 8,
+                            "color": "#666666"
+                        },
+                        {
+                            "label": "Learning Objective-C",
+                            "value": 5,
+                            "color": "#777777"
+                        },
+                        {
+                            "label": "Public speaking",
+                            "value": 3,
+                            "color": "#888888"
+                        },
+                        {
+                            "label": "Donald Rumsfeld",
+                            "value": 4,
+                            "color": "#999999"
+                        },
+                        {
+                            "label": "The Zombie Apocalypse",
+                            "value": 4,
+                            "color": "#cb2121"
+                        },
+                        {
+                            "label": "The City of Winnipeg *",
+                            "value": 3,
+                            "color": "#830909"
+                        },
+                        {
+                            "label": "IE 6",
+                            "value": 2,
+                            "color": "#923e99"
+                        },
+                        {
+                            "label": "Planes with/out snakes",
+                            "value": 5,
+                            "color": "#ae83d5"
+                        },
+                        {
+                            "label": "Off-by-one errors",
+                            "value": 3,
+                            "color": "#111111"
+                        },
+                        {
+                            "label": "Chickadees",
+                            "value": 4,
+                            "color": "#050505"
+                        },
+                        {
+                            "label": "A never-ending Harper Government",
+                            "value": 1,
+                            "color": "#646464"
+                        },
+                        {
+                            "label": "Canada",
+                            "value": 4,
+                            "color": "#747474"
+                        }
+                    ]
+                },
+                "labels": {
+                    "outer": {
+                        "format": "label-percentage1",
+                        "pieDistance": 20
+                    },
+                    "inner": {
+                        "format": "none"
+                    },
+                    "mainLabel": {
+                        "fontSize": 11
+                    },
+                    "percentage": {
+                        "color": "#999999",
+                        "fontSize": 11,
+                        "decimalPlaces": 0
+                    },
+                    "value": {
+                        "color": "#cccc43",
+                        "fontSize": 11
+                    },
+                    "lines": {
+                        "enabled": true,
+                        "color": "#777777"
+                    },
+                    "truncation": {
+                        "enabled": true
+                    }
+                },
+                "effects": {
+                    "pullOutSegmentOnClick": {
+                        "effect": "linear",
+                        "speed": 400,
+                        "size": 8
+                    }
+                },
+                "misc": {
+                    "colors": {
+                        "segmentStroke": "#000000"
+                    }
+                }
+            });
+        }
 
     });
--- public/js/require.config.js
+++ public/js/require.config.js
@@ -2,12 +2,11 @@ var requireConfig = {
     paths: {
         text: '../bower_components/requirejs-plugins/lib/text',
         json: '../bower_components/requirejs-plugins/src/json',
-        //optional: 'bower_components/requirejs-optional/dist/optional',
         lodash: '../bower_components/lodash/lodash',
         Ractive: '../bower_components/ractive/ractive',
-        socketio: '../bower_components/socket.io-client/socket.io',
         jquery: '../bower_components/jquery/dist/jquery',
-        //Cookies: '../bower_components/js-cookie/src/js.cookie',
+        d3: '../bower_components/d3/d3',
+        d3pie: '../bower_components/d3pie/d3pie/d3pie',
         views: '../views',
         res: '../res'
     }
